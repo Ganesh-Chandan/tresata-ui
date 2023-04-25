2d1261da4ea827d8bd3ea83211f3ffce588eacd5
Ganesh Chandan <ganesh.chandan@wissen.com>
2023-04-25 12:18:11 +0530

    Project folder

---
+++ src/components/header/index.tsx
@@ -0,0 +1,18 @@
+import Toggle from "../custom/Toggle";
+
+const Header = () => {
+  return (
+    <div className="app-header">
+      <div>
+        <img
+          src="../image/logo.png"
+          alt="Tresata Logo"
+          width="152"
+          height="25"
+        ></img>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
