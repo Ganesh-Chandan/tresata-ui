51a8f17fc6af24b78980dd27cf5e9cf0459a7820
Ganesh Chandan <ganesh.chandan@wissen.com>
2023-04-26 15:36:39 +0530

    Middle panel changes

---
+++ src/components/custom/Widget/index.tsx
@@ -0,0 +1,19 @@
+import styles from "./widget.module.scss";
+
+interface IWidget {
+  title: string;
+  value: string;
+  description: string;
+}
+
+const Widget = ({ title, value, description }: IWidget) => {
+  return (
+    <div className={styles.widget}>
+      <div className={styles.widgetTitle}>{title}</div>
+      <div className={styles.widgetValue}>{value}</div>
+      <div className={styles.widgetDescription}>{description}</div>
+    </div>
+  );
+};
+
+export default Widget;
