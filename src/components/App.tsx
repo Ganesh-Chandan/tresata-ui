d2e4324d6e1883fcf100c143191f2a24b1fcda89
Ganesh Chandan <ganesh.chandan@wissen.com>
2023-04-26 22:37:00 +0530

    CSS issue fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,20 @@
 import Header from "./header";
+import { Footer } from "./footer";
+import { Profiles } from "./profiles";
+import { ProfileContent } from "./profile-content";
 
 const App = () => {
   return (
-    <div className="app">
+    <div className="app" role="app">
       <Header />
+      <div className="profile-detail">
+        <header className="profile-header">Profile Sources (02)</header>
+        <div className="profile-section">
+          <Profiles />
+          <ProfileContent />
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
